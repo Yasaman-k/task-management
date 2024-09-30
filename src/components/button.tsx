d8d8b9d5882604d1b5094f11efbf26b2f5f787d4
Yasaman Karbasi <work.yasaman.me@gmail.com>
2024-09-30 17:00:46 +0330

    chore: create button component style

---
+++ src/components/button.tsx
@@ -0,0 +1,15 @@
+interface props {
+    name: string
+}
+const Button = ({ name }: props) => {
+    return (
+        <>
+            <button className=" bg-slate-500 rounded-lg p-2 text-white hover:bg-slate-400">
+                {name}
+            </button>
+
+        </>
+
+    )
+}
+export default Button;
