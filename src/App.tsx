b13f3ec69575d10a51f44871012f966c36e4e7a4
Yasaman Karbasi <work.yasaman.me@gmail.com>
2024-10-01 18:54:40 +0330

    chore: add counter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Button from './components/button';
 import Input from './components/input';
 import Textarea from './components/textarea';
+import Counter from './features/counter/Counter';
 import { staticText } from './staticText';
 
 function App() {
@@ -15,9 +16,10 @@ function App() {
 
       </header>
       <main>
+        <Counter />
         <div className='m-auto flex flex-col space-y-4 w-full md:w-1/3'>
           <h2>Add new Task here</h2>
-          <form action="" onSubmit={handleSubmit} >
+          <form action="" onSubmit={handleSubmit}>
             <div className='flex flex-col space-y-3'>
               <Input type={'text'} name={'title'} />
               <Textarea name={'description'} />
@@ -26,7 +28,6 @@ function App() {
           </form>
         </div>
       </main>
-
     </div>
   );
 }
