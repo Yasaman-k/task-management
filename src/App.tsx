ec0ac5806f3ebfdd65a3532b5aa92feda069a536
Yasaman Karbasi <work.yasaman.me@gmail.com>
2024-10-05 13:15:03 +0330

    chore: rename to tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
-import TaskList from './features/task/tasksList';
-import { staticText } from './staticText';
-import AddTaskForm from './features/task/AddTaskForm';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
 
 function App() {
   return (
-    <div className="w-full p-4 space-y-4">
-      <header className='bg-slate-800 flex justify-center h-32'>
-        <h1 className="text-3xl font-bold self-center text-white">{staticText.title}</h1>
-      </header>
-
-      <main className='m-auto flex flex-col space-y-4 w-full  md:w-1/2'>
-        {/* <AddTaskForm /> */}
-
-        <div>
-          <TaskList />
-
-        </div>
-      </main>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/about" element={<About />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
